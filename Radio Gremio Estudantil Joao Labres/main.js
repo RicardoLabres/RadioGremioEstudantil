fd4ebf7f13d05e20ac59df3151c43572fa045124
RicardoLabres <125682751+RicardoLabres@users.noreply.github.com>
2023-11-23 13:06:50 -0300

    Add files via upload

---
+++ Radio Gremio Estudantil Joao Labres/main.js
@@ -0,0 +1,86 @@
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idAudio = `#som_${efeito}`;
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function tocaSomVaia(){
+        document.querySelector("#som_tecla_vaia").play();
+        }*/
+    
+
+
+//listaDeTeclas[1].onclick = tocaSomVaia; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//listaDeTeclas[1].onclick = tocaSomVaia;
+
+
+
+/*function tocaSomVaia(){
+    document.querySelector("#som_tecla_vaia").play();
+    }*/
